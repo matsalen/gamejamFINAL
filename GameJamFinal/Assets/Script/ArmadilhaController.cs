9b4252ead3f59085e0a638039ef1189da5d93dc6
Felphz <felipejoliveira2@gmail.com>
2019-12-02 17:09:58 -0300

    Inimigo feito,colocado as armadilhas...

---
+++ GameJamFinal/Assets/Script/ArmadilhaController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ArmadilhaController : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    NavMeshAgent obs;
+
+
+    void Start()
+    {
+        obs = GetComponent<NavMeshAgent>();
+
+        obs.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void Update()
+    {
+
+       // if (!obs.pathPending && obs.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+    void GotoNextPoint()
+    {
+
+        if (points.Length == 0)
+            return;
+
+
+        obs.destination = points[destPoint].position;
+
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+}
