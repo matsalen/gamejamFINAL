07c6f9ef2c03e546d56d71ba9d0765cfaa4ec1cc
Felphz <felipejoliveira2@gmail.com>
2019-12-02 14:17:51 -0300

    Voltar no tempo abilitado.

--- GameJamFinal/Assets/Script/PlayerManager.cs
+++ GameJamFinal/Assets/Script/PlayerManager.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 using UnityEngine.AI;
 public class PlayerManager : MonoBehaviour
 {
+    #region Variaveis
+    KeyCode voltar = KeyCode.Q;
     Vector3 offset;
     NavMeshAgent agent;
-
+    [SerializeField] float x;
+    [SerializeField] float z;
+    [SerializeField] float freq_Hz = 30;
+    [SerializeField] int maxFrame = 100;
+    List<Vector3> posicoes = new List<Vector3>();
+    List<Vector3> rbVelocity = new List<Vector3>();
+    List<Vector3> rbAngularVelocity = new List<Vector3>();
+    List<Quaternion> rotacoes = new List<Quaternion>();
+    [Space(15)]
+    public bool fixedupdate_50Hz = false;
+    float freq;
+    float cronometro;
+    bool voltando = false;
+    Rigidbody rb;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
-      
     {
 
-        offset = Vector3.zero;
+       
         agent = GetComponent<NavMeshAgent>();
-        offset =  transform.position - Camera.main.transform.position;
+        freq = 1 / freq_Hz;
+        rb = GetComponent<Rigidbody>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Camera.main.transform.position = transform.position - offset;
-       // Camera.main.transform.rotation = transform.rotation ;
+        Camera.main.transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z) - new Vector3(x, 0, z);
 
         if (Input.GetMouseButton(1))
         {
@@ -32,5 +47,107 @@ public class PlayerManager : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out ray, 100))
                 agent.destination = ray.point;
         }
+   
+        if (Input.GetKeyDown(voltar))
+        {
+            if (posicoes.Count > 1 && rotacoes.Count > 1)
+            {
+                voltando = true;
+                if (rb)
+                {
+                    rb.useGravity = false;
+                }
+            }
+
+        }
+
+        if (Input.GetKeyUp(voltar))
+        {
+            voltando = false;
+            if (rb)
+            {
+                rb.useGravity = true;
+                rb.velocity = rbVelocity[rbVelocity.Count - 1];
+                rb.angularVelocity = rbAngularVelocity[rbAngularVelocity.Count - 1];
+            }
+            agent.destination = transform.position;
+
+        }
+
+        if (fixedupdate_50Hz == false)
+        {
+            cronometro += Time.deltaTime;
+            if (cronometro >= freq)
+            {
+                cronometro = 0;
+                ChecarVoltar();
+            }
+
+        }
+
+        if (voltando)
+        {
+            transform.position = Vector3.Lerp(transform.position, posicoes[posicoes.Count - 1], Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotacoes[rotacoes.Count - 1], Time.deltaTime * 5f);
+
+
+        }
+     }
+
+    void FixedUpdate()
+    {
+        if (fixedupdate_50Hz)
+        {
+            ChecarVoltar();
+        }
+
+        if (rb)
+        {
+            if (voltando)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+
+    void ChecarVoltar()
+    {
+        if (!voltando)
+        {
+            posicoes.Add(transform.position);
+            rotacoes.Add(transform.rotation);
+
+            if (rb)
+            {
+                rbVelocity.Add(rb.velocity);
+                rbAngularVelocity.Add(rb.angularVelocity);
+            }
+
+            if (posicoes.Count > maxFrame)
+            {
+                posicoes.Remove(posicoes[0]);
+                rotacoes.Remove(rotacoes[0]);
+                if (rb)
+                {
+                    rbVelocity.Remove(rbVelocity[0]);
+                    rbAngularVelocity.Remove(rbAngularVelocity[0]);
+                }
+            }
+        }
+        else
+        {
+            if (posicoes.Count > 1)
+            {
+                posicoes.Remove(posicoes[posicoes.Count - 1]);
+                rotacoes.Remove(rotacoes[rotacoes.Count - 1]);
+                if (rb)
+                {
+                    rbVelocity.Remove(rbVelocity[rbVelocity.Count - 1]);
+                    rbAngularVelocity.Remove(rbAngularVelocity[rbAngularVelocity.Count - 1]);
+                }
+            }
+        }
+
     }
 }
