dfc003d8728b4a1390de9c062f08e0b47c680f62
Felphz <felipejoliveira2@gmail.com>
2019-12-02 13:47:09 -0300

    Tilemap adicionado.

---
+++ GameJamFinal/Assets/Script/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerManager : MonoBehaviour
+{
+    Vector3 offset;
+    NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+      
+    {
+
+        offset = Vector3.zero;
+        agent = GetComponent<NavMeshAgent>();
+        offset =  transform.position - Camera.main.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position = transform.position - offset;
+       // Camera.main.transform.rotation = transform.rotation ;
+
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit ray;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out ray, 100))
+                agent.destination = ray.point;
+        }
+    }
+}
