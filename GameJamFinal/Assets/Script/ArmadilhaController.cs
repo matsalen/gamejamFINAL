9ea9b12bc8847cfbee287ffff8c9900b92b257d9
Felphz <felipejoliveira2@gmail.com>
2019-12-03 15:05:39 -0300

    Armadilha funcionando

--- GameJamFinal/Assets/Script/ArmadilhaController.cs
+++ GameJamFinal/Assets/Script/ArmadilhaController.cs
@@ -5,36 +5,40 @@ using UnityEngine.AI;
 
 public class ArmadilhaController : MonoBehaviour
 {
-    public Transform[] points;
-    private int destPoint = 0;
-    NavMeshAgent obs;
-
-
-    void Start()
+    Vector3 posinicial;
+    public Transform target;
+    public float speed;
+    bool Ativar = false;
+    bool voltar = false;
+    private void Start()
     {
-        obs = GetComponent<NavMeshAgent>();
-
-        obs.autoBraking = false;
-
-        GotoNextPoint();
+        posinicial = transform.position;
     }
-
     void Update()
     {
-
-       // if (!obs.pathPending && obs.remainingDistance < 0.5f)
-            GotoNextPoint();
-    }
-    void GotoNextPoint()
-    {
-
-        if (points.Length == 0)
-            return;
-
-
-        obs.destination = points[destPoint].position;
-
-
-        destPoint = (destPoint + 1) % points.Length;
+        float step = speed * Time.deltaTime;
+        // trocar para quando pegar a bola
+        if (Input.GetButton("Jump"))
+            Ativar = true;
+        // se a bola foi pega a armadilha vai ativar
+        if (Ativar)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            if (transform.position == target.position)
+            {
+                voltar = true;
+                Ativar = false;
+            }
+        }
+        // se a armadilha ja ativou ela voltara e repetira a ativação
+        if (voltar)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posinicial, step);
+            if (transform.position == posinicial)
+            {
+                voltar = false;
+                Ativar = true;
+            }
+        }
     }
 }
